ef413f27427921cbb0fa1c8249f206fa4e3da007
nheisler <42762547+nheisler@users.noreply.github.com>
2022-08-31 18:31:49 -0600

    Merged game into one page and added buttons and forms

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask, render_template
 import pandas
 import random
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
-df = pandas.read_csv('clues.csv')
-index1 = random.randint(0, len(df.index))
-index2 = random.randint(0, len(df.index))
-index3 = random.randint(0, len(df.index))
-index4 = random.randint(0, len(df.index))
+def setData():
+    global df
+    global index1
+    global index2
+    global index3
+    global index4
+
+    df = pandas.read_csv('clues.csv')
+    index1 = random.randint(0, (len(df.index) - 1))
+    index2 = random.randint(0, (len(df.index) - 1))
+    while index2 == index1:
+        index2 = random.randint(0, (len(df.index) - 1))
+
+    index3 = random.randint(0, (len(df.index) - 1))
+    while index3 == index1 or index3 == index2:
+        index3 = random.randint(0, (len(df.index) - 1))
+
+    index4 = random.randint(0, (len(df.index) - 1))
+    while index4 == index1 or index4 == index2 or index4 == index3:
+        index4 = random.randint(0, (len(df.index) - 1))
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=setData, trigger="cron", day_of_week='mon-sun', hour=1, minute=0)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+setData()
 
 app = Flask(__name__)
 
@@ -14,22 +40,20 @@ app = Flask(__name__)
 def main_page():
     return render_template('start.html')
 
-@app.route("/first")
-def first_page():
-    return render_template('first_value.html', hint=df["clue"][index1])
-
-@app.route("/second")
-def second_page():
-    return render_template('second_value.html', hint=df["clue"][index2])
-
-@app.route("/third")
-def third_page():
-    return render_template('third_value.html', hint=df["clue"][index3])
-
-@app.route("/fourth")
-def fourth_page():
-    return render_template('fourth_value.html', hint=df["clue"][index4])
+@app.route("/play")
+def game_page():
+    return render_template('game_page.html', hint1=df["clue"][index1], hint2=df["clue"][index2], hint3=df["clue"][index3], hint4=df["clue"][index4])
 
 @app.route("/solution")
 def solution_page():
-    return render_template('solution.html', v1=1, v2=2, v3=3, v4=4)
+    finalMessage = "Your solution was correct! The lock has been opened"
+    
+    if True:
+        finalMessage = "Your solution was incorrect! The correct combination is "
+        finalMessage += str(df["value"][index1])
+        finalMessage += str(df["value"][index2])
+        finalMessage += str(df["value"][index3])
+        finalMessage += str(df["value"][index4])
+        finalMessage += " -- Try again tomorrow"
+
+    return render_template('solution.html', d1=1, d2=2, d3=3, d4=4, message=finalMessage)
